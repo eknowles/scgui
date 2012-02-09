610c150bfc1cda9aec411577dc95c1a57108385d
eknowles9@gmail.com <eknowles9@gmail.com@5e568a55-4ddd-bd06-ed64-73a5cd320787>
2012-02-09 22:56:56 +0000

    updated things

--- trunk/main.py
+++ trunk/main.py
@@ -2,71 +2,86 @@
 import os, os.path
 import re
 import shutil
+import commands
+
 
 class scgui():
     def __init__(self):
-        self.steamname = "3ez"
-        self.skinspath = "C:\Program Files (x86)\KLUTCH\SCGUI\Skins"
-        self.installedskin = "Base"
-        self.cspath=os.path.join(os.environ["ProgramFiles(x86)"],"Steam/steamapps",self.steamname,"counter-strike source")
-        self.specsettings ={
-                              "t1name":"", 
-                              "t1url":"", 
-                              "t1flag":"", 
-                              "t2name":"", 
-                              "t2url":"", 
-                              "t2flag":"", 
-                               }
-        self.cfgsettings ={
-                              "spec_player":"", 
-                              "hud_saytext_time":"", 
-                              "hud_deathnotice_time":"", 
-                              "spec_autodirector":"", 
-                              "spec_scoreboard":"", 
-                              "spec_mode":"", 
-                              "net_graph":"",  
-                              "cl_dynamiccrosshair":"", 
-                              "func_break_max_pieces":"", 
-                              "overview_names":"", 
-                               }
+        self.steamname = "3fk"
+        self.skinspath = os.path.join(os.environ["ProgramFiles(x86)"],"SCGUI\Skins")
+        self.installedskin = {"Skin":"","Version":"","Author":"","Website":"","Info":""}
+        self.cspath=os.path.join(os.environ["ProgramFiles(x86)"],"Steam/steamapps",self.steamname,"counter-strike source/cstrike")
+        self.specsettings ={"t1name":"","t1url":"","t1flag":"","t2name":"","t2url":"","t2flag":""}
+        self.cfgsettings ={"spec_player":"","hud_saytext_time":"","hud_deathnotice_time":"","spec_autodirector":"","spec_scoreboard":"","spec_mode":"","net_graph":"","cl_dynamiccrosshair":"","func_break_max_pieces":"","overview_names":""}
         self.resfiles=["Spectator.res", "ScoreBoard.res"]
         self.cfgfiles=["KLUTCH.cfg", "autoexec.cfg"]
         
     # This function opens up the KLUTCH.cfg and gets the installed skins name (this should match a dir in the skins folder)
     def getinstalledskin(self, accountname):
-        targetfilelocation=os.path.join(self.cspath,"cstrike/cfg/KLUTCH.cfg")
-        maincfg = open(targetfilelocation,"r")
-        for line in maincfg.readlines():
-            if re.search(r"// \[[^\]]*?=[^\]]*?\]", line):
-                matches = re.search(r"// \[Skin=([^\s]*?)\]", line)
-                if matches is not None:
-                    self.installedskin = matches.group(1)
-                    print self.installedskin
+        
+        targetfilelocation=os.path.join(self.cspath,"cfg/KLUTCH.cfg")
+        if os.path.exists(targetfilelocation):
+            maincfg = open(targetfilelocation,"r")
+            for line in maincfg.readlines():
+                if re.search(r"// \[[^\]]*?=[^\]]*?\]", line):
+                    matches = re.search(r"// \[([^\s]*?)=(.*?)\]", line)
+                    if matches is not None:
+                        self.installedskin[matches.group(1)] = matches.group(2)
+        else:
+            print 'skin not installed.'
+        
+        
     
     # This will remove all skins from the cstrike dir
     def uninstallskin(self):
+        currentskindir = os.path.join(self.skinspath,self.installedskin["Skin"])
         
+        for subdir, dirs, files in os.walk(currentskindir):
+            for name in files:
+                filepath = os.path.join(subdir,name)
+                filepathrel = os.path.relpath(filepath, currentskindir)
+                filepathmain = os.path.join(self.cspath, filepathrel)
+                if os.path.exists(filepathmain):
+                    os.remove(filepathmain)
+                    print "file "+filepathmain+" deleted."
+                filebasemain = os.path.dirname(filepathmain)    
+                if os.path.exists(filebasemain) and os.listdir(filebasemain) == []:
+                    os.rmdir(filebasemain)
+                    print "folder "+filebasemain+" deleted." 
+        print self.installedskin
+    
+    
+    
+    
     
     # This removes the current installed skins and will copy the desired skin into the game folder
-    def installskin(src):    
-        names = os.listdir(src)
-        if not os.path.exists(self.cspath): # no error if already exists
-            os.makedirs(dst)
+    def installskin(self, desiredskin):                  
+        desiredskindir = os.path.join(self.skinspath, desiredskin) 
+        #DesiredSkinFiles = os.listdir(desiredskindir)
+        #print desiredskindir
+        
+        for subdir, dirs, files in os.walk(desiredskindir):
+            for name in files:
+                print name
+                        
+        if not os.path.exists(desiredskindir): # no error if already exists
+            os.makedirs(desiredskin)
         errors = []
-        for name in names:
-            srcname = os.path.join(src, name)
-            dstname = os.path.join(dst, name)
+        for skinfile in DesiredSkinFiles:
+            srcfile = os.path.join(desiredskindir, skinfile)
+            dstfile = os.path.join(self.cspath, skinfile)
+            print srcfile
             try:
-                if os.path.isdir(srcname):
-                    shutil.copytree(srcname, dstname)
+                if os.path.isdir(srcfile):
+                    shutil.copytree(srcfile, dstfile)
                 else:
-                    shutil.copy2(srcname, dstname)
+                    shutil.copy2(srcfile, dstfile)
             except (IOError, os.error), why:
-                errors.append((srcname, dstname, str(why)))
-            
+                errors.append((srcfile, dstfile, str(why)))
+#            
     # This reads resorce files and puts key settings into variables
     def resfileinterp(self, targetfile):
-        targetfilelocation=os.path.join(self.cspath,"cstrike/resource/UI",targetfile)
+        targetfilelocation=os.path.join(self.cspath,"resource/UI",targetfile)
         resfile = open(targetfilelocation,"r")
         for line in resfile.readlines():
             if re.search(r"\"[^\"]*\"[\s]*?\"[^\"]*\"[\s]*?//%[^%]*%", line):
@@ -74,7 +89,7 @@ class scgui():
                 self.specsettings[matches.group(3)] = matches.group(2)
                 
     def cfgfileinterp(self,targetfile):
-        targetfilelocation=os.path.join(self.cspath,"cstrike/cfg/KLUTCH.cfg")
+        targetfilelocation=os.path.join(self.cspath,"cfg/KLUTCH.cfg")
         cfgfile = open(targetfilelocation,"r")
         for line in cfgfile.readlines():
             if re.search(r"\"[^\"]*\"[\s]*?\"[^\"]*\"", line):
@@ -82,7 +97,7 @@ class scgui():
                 self.cfgsettings[matches.group(1)] = matches.group(2)
     
     def editres(self,targetfile):
-        targetfilelocation=os.path.join(self.cspath,"cstrike/resource/UI",targetfile)
+        targetfilelocation=os.path.join(self.cspath,"resource/UI",targetfile)
         resfile = open(targetfilelocation,"r")
         resfile_output = resfile.read();
         resfile.close()
@@ -94,7 +109,7 @@ class scgui():
         #print resfile_output    
         
     def editcfg(self,targetfile):
-        targetfilelocation=os.path.join(self.cspath,"cstrike/cfg/KLUTCH.cfg")
+        targetfilelocation=os.path.join(self.cspath,"cfg/KLUTCH.cfg")
         cfgfile = open(targetfilelocation,"r")
         cfgfile_output = cfgfile.read();
         cfgfile.close()
@@ -111,6 +126,9 @@ class scgui():
             self.resfileinterp(guifile)
         for guifile in self.cfgfiles:
             self.cfgfileinterp(guifile)
+            
+    def launchcs(self):
+        os.execv("C:\Program Files (x86)\Steam\Steam.exe", ['-applaunch 240','-novid','-sw','-w 1280','-h 720','-x 0','-y 0','-noborder','+fps_max 240'])
 
 makedo=scgui()
 #makedo.readfiles()
@@ -124,6 +142,8 @@ makedo=scgui()
 #print makedo.specsettings
 #print makedo.cfgsettings
 makedo.getinstalledskin(makedo.steamname)
-
+#makedo.uninstallskin()
+#makedo.installskin('wolves')
 #makedo.editcfg(makedo.cfgfiles[1])
 #makedo.editres(makedo.resfiles[0])
+#makedo.launchcs()
